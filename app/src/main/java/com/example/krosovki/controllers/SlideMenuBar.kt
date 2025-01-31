e5d4c9261564a08965a4a578e5989eadf076d142
Student <Student@tkfk.local>
2025-01-31 15:40:28 +0500

    Updating SlideBar

--- app/src/main/java/com/example/krosovki/controllers/SlideMenuBar.kt
+++ app/src/main/java/com/example/krosovki/controllers/SlideMenuBar.kt
@@ -2,9 +2,13 @@ package com.example.krosovki.controllers
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +18,13 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.DeliveryDining
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Logout
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.ShoppingBag
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -29,6 +40,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -48,11 +60,14 @@ fun SlideMenuBar(
 
     ModalNavigationDrawer(
         drawerContent = {
-            ModalDrawerSheet {
+            ModalDrawerSheet(
+                modifier = Modifier.fillMaxSize(),
+                drawerContainerColor = Color(0xFF48B2E7)
+            ) {
                 Column(
                     modifier = Modifier.padding(horizontal = 16.dp)
                         .verticalScroll(rememberScrollState())
-                        .background(Color(0xFF48B2E7))
+
                 ) {
                     Spacer(Modifier.height(12.dp))
                     Image(
@@ -64,49 +79,51 @@ fun SlideMenuBar(
 
                     Text("Человек яйца", modifier = Modifier.padding(16.dp))
 
-
-                    Text("Section 1", modifier = Modifier.padding(16.dp))
                     NavigationDrawerItem(
-                        label = { Text("Профиль") },
+                        icon = { Icon(Icons.Outlined.Person, "Person") },
+                        label = { Text(text = "Профиль") },
                         selected = false,
                         onClick = {}
                     )
                     NavigationDrawerItem(
-                        label = { Text("Корзина") },
+                        icon = { Icon(Icons.Outlined.ShoppingBag, "Cart") },
+                        label = { Text(text = "Корзина") },
                         selected = false,
                         onClick = {}
                     )
                     NavigationDrawerItem(
-                        label = { Text("Избранное") },
+                        icon = { Icon(Icons.Outlined.FavoriteBorder, "add") },
+                        label = { Text(text = "Избранное") },
                         selected = false,
                         onClick = {}
                     )
                     NavigationDrawerItem(
-                        label = { Text("Заказы") },
+                        icon = { Icon(Icons.Outlined.DeliveryDining, "Deliver") },
+                        label = { Text(text = "Заказы") },
                         selected = false,
                         onClick = {}
                     )
                     NavigationDrawerItem(
-                        label = { Text("Уведомления") },
+                        icon = { Icon(Icons.Outlined.Notifications, "Notifications") },
+                        label = { Text(text = "Уведомления") },
                         selected = false,
                         onClick = {}
                     )
                     NavigationDrawerItem(
-                        label = { Text("Настройки") },
+                        icon = { Icon(Icons.Outlined.Settings, "Settings") },
+                        label = { Text(text = "Настройки") },
                         selected = false,
                         onClick = {}
                     )
 
                     HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
 
-                    Text("Section 2", modifier = Modifier.padding(16.dp))
                     NavigationDrawerItem(
-                        label = { Text("Выйти") },
+                        icon = { Icon(Icons.Outlined.Logout, "Logout") },
+                        label = {  Text(text = "Выйти") },
                         selected = false,
                         onClick = {}
                     )
-
-                    Spacer(Modifier.height(12.dp))
                 }
             }
         }
